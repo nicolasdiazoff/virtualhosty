e474b5d8876d16430382d157ad33205bcc0f59de
Nicolas Diaz <nicolasdiazoff@gmail.com>
2018-01-14 16:36:52 -0500

    hola mundo

--- controllers/read.js
+++ controllers/read.js
@@ -3,45 +3,48 @@ var fs = require('fs'),
 
 
 function projectRead(){
-	fs.readdir("C:/ark/", function(err, files){
-        var proyectos = new Array;
-        var phaty = "C:/ark/";
-        for(var i = 0; i < files.length ; i++){    
-            fs.readFile(phaty + files[i] + "/package.json", "utf8", function(err, data){
-                console.log(JSON.parse(data));
-                proyectos.push(JSON.parse(data));
 
-                // if (err === 'ENOENT') {
-                //         console.log('File not found!');                        
-                //     }
-                //     else if(data === undefined){
-                //         console.log('File not found!');                        
-                //     }
-                //     else{
-                //         var project = JSON.parse(data);
-                //         console.log(project.name);
-                //     }
+    var phaty = "C:/ark/";
+    var files = fs.readdirSync(phaty);
+    var proyectos = new Array;
+    
+    for(var i = 0; i < files.length ; i++){    
+        fs.readFileSync(phaty + files[i] + "/package.json"
+            // , "utf8", function(err, data){
+            //     throw err
+            //     if (err === 'ENOENT') {
+            //         console.log(err);                        
+            //     }
+            //     else if(data === undefined){
+            //         // console.log('File not found!');                        
+            //     }
+            //     else{
+            //         var project = JSON.parse(data);
+            //         // console.log(project.name);
+            //         proyectos.push(project.name);
+            //         console.log(proyectos);
+            //     }
+            // }
+        );
 
-                }
-            );
-        }
+    }
 
-        console.log(proyectos);
+    console.log(proyectos);
 
-        const promptList = [
-            {
-                type: "list",
-                name: "projects",
-                message: "Escoge un proyecto para abrirlo:",
-                choices: proyectos
-            }
-        ];
 
-        inquirer.prompt(promptList).then(function(ans){
-            console.log("Open");
-        });
+    // const promptList = [
+    //     {
+    //         type: "list",
+    //         name: "projects",
+    //         message: "Escoge un proyecto para abrirlo:",
+    //         choices: proyectos
+    //     }
+    // ];
 
-    });
+    // inquirer.prompt(promptList).then(function(ans){
+    //     console.log("Open");
+    // });
+
 
 }
 
